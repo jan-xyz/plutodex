4ebfe6266280bfe6ceaa35775516c0ee3be47064
Jan Steinke <jan.steinke@moia.io>
2022-12-07 19:25:53 +0100

    relative URLs

--- moiadex/vite.config.ts
+++ moiadex/vite.config.ts
@@ -4,70 +4,71 @@ import inlineCssModules from 'vite-plugin-inline-css-modules'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  publicDir: 'assets',
-  plugins: [
-    solidPlugin(),
-    inlineCssModules(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'MOIA Plutodex',
-        short_name: 'Plutodex',
-        theme_color: '#E6AA33',
-        background_color: '#F0ECE4',
-        display: 'standalone',
-        scope: './',
-        start_url: './',
-        icons: [
-          {
-            src: 'icons/icon-72x72.png',
-            sizes: '72x72',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-96x96.png',
-            sizes: '96x96',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-128x128.png',
-            sizes: '128x128',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-152x152.png',
-            sizes: '152x152',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-          },
-          {
-            src: 'icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+	publicDir: 'assets',
+	base: '',
+	plugins: [
+		solidPlugin(),
+		inlineCssModules(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+			manifest: {
+				name: 'MOIA Plutodex',
+				short_name: 'Plutodex',
+				theme_color: '#E6AA33',
+				background_color: '#F0ECE4',
+				display: 'standalone',
+				scope: './',
+				start_url: './',
+				icons: [
+					{
+						src: 'icons/icon-72x72.png',
+						sizes: '72x72',
+						type: 'image/png',
+					},
+					{
+						src: 'icons/icon-96x96.png',
+						sizes: '96x96',
+						type: 'image/png',
+					},
+					{
+						src: 'icons/icon-128x128.png',
+						sizes: '128x128',
+						type: 'image/png',
+					},
+					{
+						src: 'icons/icon-144x144.png',
+						sizes: '144x144',
+						type: 'image/png',
+					},
+					{
+						src: 'icons/icon-152x152.png',
+						sizes: '152x152',
+						type: 'image/png',
+					},
+					{
+						src: 'icons/icon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'icons/icon-384x384.png',
+						sizes: '384x384',
+						type: 'image/png',
+					},
+					{
+						src: 'icons/icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+		}),
+	],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: 'esnext',
+	},
 })
