6971aa10184b7cbeb968c89af187098e66920d80
sanderdrummer <tobias.pickel@moia.io>
2022-12-07 12:11:59 +0100

    feat: add vite pwa

--- moiadex/src/App.tsx
+++ moiadex/src/App.tsx
@@ -1,5 +1,18 @@
 import type { Component } from 'solid-js'
 import { MoiaDexRoutes } from './MoiaDexRoutes'
+import { useRegisterSW } from 'virtual:pwa-register/solid'
+
+const intervalMS = 60 * 60 * 1000
+
+useRegisterSW({
+  onRegistered(r) {
+    r &&
+      setInterval(() => {
+        r.update()
+        console.info('update')
+      }, intervalMS)
+  },
+})
 
 const App: Component = () => {
   return (
--- moiadex/vite.config.ts
+++ moiadex/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import inlineCssModules from 'vite-plugin-inline-css-modules'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [solidPlugin(), inlineCssModules()],
+  plugins: [
+    solidPlugin(),
+    inlineCssModules(),
+    VitePWA({
+      registerType: 'autoUpdate',
+    }),
+  ],
   server: {
     port: 3000,
   },
