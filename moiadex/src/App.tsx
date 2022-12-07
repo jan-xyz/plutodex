e60f8fff0146fa2191cf94a47a3167f15b5ed7d5
sanderdrummer <tobias.pickel@moia.io>
2022-12-07 15:06:37 +0100

    feat: add search

--- moiadex/src/App.tsx
+++ moiadex/src/App.tsx
@@ -1,7 +1,7 @@
 import type { Component } from 'solid-js'
 import { MoiaDexRoutes } from './MoiaDexRoutes'
 import { useRegisterSW } from 'virtual:pwa-register/solid'
-
+import './reset.css'
 const intervalMS = 60 * 60 * 1000
 
 useRegisterSW({
