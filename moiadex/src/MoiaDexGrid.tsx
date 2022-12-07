0b15dee31672355ccedfc98e8e5b492e5f770a09
Juan Jose Lenero Lozano <juan-jose.lozano@moia.io>
2022-12-07 15:16:21 +0100

    first pass at implementing design for moia dex grid

--- moiadex/src/MoiaDexGrid.tsx
+++ moiadex/src/MoiaDexGrid.tsx
@@ -2,13 +2,14 @@ import { A } from '@solidjs/router'
 import { For } from 'solid-js'
 import { css } from 'vite-plugin-inline-css-modules'
 import { Moia } from './moiaDexie'
-import { MoiaDexItemView } from './MoiaDexItemView'
+import { MoiaDexGridItem } from './MoiaDexItemView'
 
 const GridStyle = css`
   .dexGrid {
-    color: hotpink;
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+    background: #F0ECE4;
+    border-radius: 35px;
   }
 `
 
@@ -18,7 +19,7 @@ export const MoiaDexGrid = (props: { moias: Moia[] }) => {
       <For each={props.moias}>
         {(moia) => (
           <A href={`/moia/${moia.id}`}>
-            <MoiaDexItemView label={moia.label} counter={moia.counter} />
+            <MoiaDexGridItem label={moia.label} counter={moia.counter} />
           </A>
         )}
       </For>
