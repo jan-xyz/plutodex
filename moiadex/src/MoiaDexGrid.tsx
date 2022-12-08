400d540c595e2e1636a284601e6e82698d87968e
sanderdrummer <tobias.pickel@moia.io>
2022-12-08 10:33:57 +0100

    fix: audio not working on mobile

--- moiadex/src/MoiaDexGrid.tsx
+++ moiadex/src/MoiaDexGrid.tsx
@@ -1,4 +1,4 @@
-import { For } from 'solid-js'
+import { For, JSX } from 'solid-js'
 import { css } from 'vite-plugin-inline-css-modules'
 import { Moia, tagMoiaAsSeen } from './moiaDexie'
 import { MoiaDexGridItem } from './MoiaDexItemView'
@@ -10,38 +10,49 @@ const GridStyle = css`
     grid-template-columns: repeat(auto-fill, 98px);
     justify-content: center;
     gap: 20px;
+    width: 100%;
+    min-height: calc(100vh - 400px);
+  }
+  .container {
     background: #f0ece4;
     border-top-left-radius: 35px;
     border-top-right-radius: 35px;
     width: 100%;
     padding: 35px 25px;
-    min-height: calc(100vh - 400px);
   }
 `
 
+export const GridContainer = (props: { children: JSX.Element }) => {
+  return <div class={GridStyle.container}>{props.children}</div>
+}
+
 export const MoiaDexGrid = (props: { moias: Moia[]; refetch }) => {
-  const tagAudio = makeAudio("/seen.wav");
+  const tagAudio = makeAudio('/seen.wav')
 
   return (
-    <div class={GridStyle.dexGrid}>
-      {!props?.moias && <p> Loading Moias...</p>}
-      <For each={props.moias}>
-        {(moia) => (
-          <div
-            onClick={async () => {
-              await tagMoiaAsSeen(moia.id.toString())
-              await tagAudio.play()
-              props.refetch()
-            }}
-          >
-            <MoiaDexGridItem
-              label={moia.id.toString()}
-              counter={moia.counter}
-              type={moia.type}
-            />
-          </div>
-        )}
-      </For>
-    </div>
+    <GridContainer>
+      <div class={GridStyle.dexGrid}>
+        {!props?.moias && <p> Loading Moias...</p>}
+        <For each={props.moias}>
+          {(moia) => (
+            <div
+              onClick={async () => {
+                await tagMoiaAsSeen(moia.id.toString())
+                try {
+                  await tagAudio.play()
+                } catch { }
+                props.refetch()
+              }}
+            >
+              <MoiaDexGridItem
+                label={moia.id.toString()}
+                counter={moia.counter}
+                type={moia.type}
+              />
+            </div>
+          )}
+        </For>
+      </div>
+    </GridContainer>
   )
 }
